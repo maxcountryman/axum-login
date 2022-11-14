66d1784d0a54880fb44b500ea3743e4934099c0b
Paweł Jan Czochański <pawel@czochanski.pl>
2022-11-14 19:27:44 +0100

    Remove std::fmt::Debug from AuthUser requirements
    This PR fixes #6 and improves the `password_hash` security hygiene.

--- src/user_store.rs
+++ src/user_store.rs
@@ -5,7 +5,7 @@ use crate::{AuthUser, Result};
 /// A trait which defines a method that allows retrieval of users from an
 /// arbitrary backend.
 #[async_trait]
-pub trait UserStore: std::fmt::Debug + Clone + Send + Sync + 'static {
+pub trait UserStore: Clone + Send + Sync + 'static {
     /// An associated user type which will be loaded from the store.
     type User: AuthUser;
 
