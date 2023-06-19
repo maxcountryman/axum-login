e26f2c70b8b984686234d6ad6a98460f4332f06c
Paweł Czochański <pawel@czochanski.pl>
2023-06-19 16:26:10 +0200

    Add DefaultQueryProvider (#72)

--- axum-login/src/sqlx_store.rs
+++ axum-login/src/sqlx_store.rs
@@ -13,26 +13,54 @@ use sqlx::{Encode, FromRow, Type};
 
 use crate::{user_store::UserStore, AuthUser};
 
+pub trait DefaultQueryProvider: Clone {
+    fn default_query() -> String;
+}
+
+#[derive(Clone)]
+pub struct SqlxQueryProvider;
+
+impl DefaultQueryProvider for SqlxQueryProvider {
+    fn default_query() -> String {
+        "SELECT * FROM users WHERE id = $1".to_string()
+    }
+}
+
+#[derive(Clone)]
+pub struct MySqlQueryProvider;
+
+impl DefaultQueryProvider for MySqlQueryProvider {
+    fn default_query() -> String {
+        "SELECT * FROM users WHERE id = ?".to_string()
+    }
+}
+
 /// A generic SQLx user store.
 ///
 /// Concrete implementations are provided as well and should usually be used
 /// unless generics are required by the application.
 #[derive(Clone, Debug)]
-pub struct SqlxStore<Pool, User, Role = ()> {
+pub struct SqlxStore<Pool, User, Role = (), QueryProvider: DefaultQueryProvider = SqlxQueryProvider>
+{
     pool: Pool,
     query: String,
     _user_type: PhantomData<User>,
     _role_type: PhantomData<Role>,
+    _default_query_provider_type: PhantomData<QueryProvider>,
 }
 
-impl<Pool, User, Role> SqlxStore<Pool, User, Role> {
+impl<Pool, User, Role, QueryProvider> SqlxStore<Pool, User, Role, QueryProvider>
+where
+    QueryProvider: DefaultQueryProvider,
+{
     /// Creates a new store with the provided pool.
     pub fn new(pool: Pool) -> Self {
         Self {
             pool,
-            query: "SELECT * FROM users WHERE id = $1".to_string(),
+            query: QueryProvider::default_query(),
             _user_type: Default::default(),
             _role_type: Default::default(),
+            _default_query_provider_type: Default::default(),
         }
     }
 
@@ -77,7 +105,7 @@ pub type MssqlStore<User, Role = ()> = SqlxStore<MssqlPool, User, Role>;
 
 /// A MySql user store via sqlx.
 #[cfg(feature = "mysql")]
-pub type MySqlStore<User, Role = ()> = SqlxStore<MySqlPool, User, Role>;
+pub type MySqlStore<User, Role = ()> = SqlxStore<MySqlPool, User, Role, MySqlQueryProvider>;
 
 /// A Postgres user store via sqlx.
 #[cfg(feature = "postgres")]
@@ -101,7 +129,10 @@ mod tests {
     use secrecy::SecretVec;
     use sqlx::SqlitePool;
 
-    use crate::{AuthUser, SqliteStore};
+    use crate::{
+        sqlx_store::{DefaultQueryProvider, MySqlQueryProvider},
+        AuthUser, SqliteStore,
+    };
 
     #[derive(Debug, Default, Clone, sqlx::FromRow)]
     struct User {
@@ -123,6 +154,12 @@ mod tests {
     async fn test_store_without_query_override_has_default_query(pool: SqlitePool) {
         let store = SqliteStore::<User>::new(pool);
         assert_eq!(store.query, "SELECT * FROM users WHERE id = $1".to_string());
+    }
+
+    #[sqlx::test]
+    async fn test_mysql_store_without_query_override_has_default_query() {
+        let query: String = MySqlQueryProvider::default_query();
+        assert_eq!(query, "SELECT * FROM users WHERE id = ?".to_string());
     }
 
     #[sqlx::test]
