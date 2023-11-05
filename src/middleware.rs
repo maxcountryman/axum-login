845427e8d8797700bcde6e3651cb7e65f649fb57
Max Countryman <maxc@me.com>
2023-11-05 10:40:15 -0800

    improve macros a bit

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,53 +1,56 @@
 #[macro_export]
 macro_rules! login_required {
     ($backend_type:ty, login_url = $login_url:expr) => {
-        $crate::predicate_required!(
+        $crate::login_required!(
             $backend_type,
-            $login_url,
-            "next",
-            |auth_session: $crate::AuthSession<$backend_type>| async move {
-                auth_session.user.is_some()
-            }
-        );
+            login_url = $login_url,
+            redirect_field = "next"
+        )
     };
 
-    ($backend_type:ty, login_url = $login_url:expr, redirect_field = $redirect_field:expr) => {
-        $crate::predicate_required!(
-            $backend_type,
-            $login_url,
-            $redirect_field,
-            |auth_session: $crate::AuthSession<$backend_type>| async move {
-                auth_session.user.is_some()
-            }
-        );
-    };
+    ($backend_type:ty, login_url = $login_url:expr, redirect_field = $redirect_field:expr) => {{
+        async fn is_authenticated(
+            auth_session: $crate::AuthSession<$backend_type>,
+        ) -> Result<bool, ()> {
+            Ok(auth_session.user.is_some())
+        }
+
+        $crate::predicate_required!($backend_type, $login_url, $redirect_field, is_authenticated)
+    }};
 }
 
 #[macro_export]
 macro_rules! permission_required {
-    ($backend_type:ty,  login_url = $login_url:expr, $($perm:expr),+) => {
+    ($backend_type:ty, login_url = $login_url:expr, redirect_field = $redirect_field:expr, $($perm:expr),+) => {{
+        async fn is_authorized(auth_session: $crate::AuthSession<$backend_type>) -> Result<bool, ::http::StatusCode> {
+            if let Some(ref user) = auth_session.user {
+                let mut has_all_permissions = true;
+                $(
+                    has_all_permissions = has_all_permissions &&
+                        auth_session.backend.has_perm(user, $perm).await.unwrap_or(false);
+                )+
+                Ok(has_all_permissions)
+            } else {
+                Ok(false)
+            }
+        }
+
         $crate::predicate_required!(
             $backend_type,
             $login_url,
-            "next",
-            |auth_session: $crate::AuthSession<$backend_type>| async move {
-                if let Some(ref user) = auth_session.user {
-                    let mut has_all_permissions = true;
-                    $(
-                        has_all_permissions = has_all_permissions && auth_session.backend.has_perm(user, $perm).await.unwrap_or(false);
-                    )+
-                    has_all_permissions
+            $redirect_field,
+            is_authorized
+        )
+    }};
 
-                } else {
-                    false
-                }
-            }
-        );
-    };
-
-    ($backend_type:ty, login_url = $login_url:expr, $perms:expr) => {
-        $crate::permission_required!($backend_type, login_url = $login_url, $perms);
-    };
+    ($backend_type:ty, login_url = $login_url:expr, $($perm:expr),+) => {{
+        $crate::permission_required!(
+            $backend_type,
+            login_url = $login_url,
+            redirect_field = "next",
+            $($perm),+
+        )
+    }};
 }
 
 #[macro_export]
@@ -60,13 +63,15 @@ macro_rules! predicate_required {
 
         from_fn(
             |auth_session: $crate::AuthSession<$backend_type>, req, next: Next<_>| async move {
-                if $predicate(auth_session).await {
-                    next.run(req).await
-                } else {
-                    let uri = req.uri().to_string();
-                    let next = urlencoding::encode(&uri);
-                    let redirect_url = format!("{}?{}={}", $login_url, $redirect_field, next);
-                    Redirect::to(&redirect_url).into_response()
+                match $predicate(auth_session).await {
+                    Ok(true) => next.run(req).await,
+                    Ok(false) => {
+                        let uri = req.uri().to_string();
+                        let next = urlencoding::encode(&uri);
+                        let redirect_url = format!("{}?{}={}", $login_url, $redirect_field, next);
+                        Redirect::to(&redirect_url).into_response()
+                    }
+                    Err(res) => res.into_response(),
                 }
             },
         )
