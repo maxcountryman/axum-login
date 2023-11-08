6d752577d20551aa9c68e79de6ee36c285cf4dc8
Max Countryman <maxc@me.com>
2023-11-08 15:09:36 -0800

    clean up a bit

--- examples/sqlite/src/main.rs
+++ examples/sqlite/src/main.rs
@@ -3,18 +3,15 @@
 //! ```not_rust
 //! cargo run -p example-sqlite
 //! ```
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use crate::web::App;
 
 mod users;
 mod web;
 
-// User and backend.
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
-
     tracing_subscriber::registry()
         .with(EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(
             |_| "axum_login=debug,sqlx=warn,tower_http=debug".into(),
