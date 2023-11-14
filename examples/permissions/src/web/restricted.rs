08d10da69e786849db00b00774c0eed3cd4206f4
Max Countryman <maxc@me.com>
2023-11-14 15:31:11 -0800

    provide permissions example (#117)

---
+++ examples/permissions/src/web/restricted.rs
@@ -0,0 +1,30 @@
+use askama::Template;
+use axum::{response::IntoResponse, routing::get, Router};
+use http::StatusCode;
+
+use crate::users::AuthSession;
+
+#[derive(Template)]
+#[template(path = "restricted.html")]
+struct RestrictedTemplate<'a> {
+    username: &'a str,
+}
+
+pub fn router() -> Router<()> {
+    Router::new().route("/restricted", get(self::get::restricted))
+}
+
+mod get {
+    use super::*;
+
+    pub async fn restricted(auth_session: AuthSession) -> impl IntoResponse {
+        match auth_session.user {
+            Some(user) => RestrictedTemplate {
+                username: &user.username,
+            }
+            .into_response(),
+
+            None => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        }
+    }
+}
