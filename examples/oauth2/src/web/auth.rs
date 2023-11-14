8816818a5d68e1a8416dac5a3317a99c101fbc58
Max Countryman <maxc@me.com>
2023-11-14 15:30:40 -0800

    provide oauth2 example (#114)

---
+++ examples/oauth2/src/web/auth.rs
@@ -0,0 +1,81 @@
+use askama::Template;
+use axum::{
+    extract::Query,
+    response::{IntoResponse, Redirect},
+    routing::{get, post},
+    Form, Router,
+};
+use http::StatusCode;
+use serde::Deserialize;
+use tower_sessions::Session;
+
+use crate::{users::AuthSession, web::oauth::CSRF_STATE_KEY};
+
+pub const NEXT_URL_KEY: &str = "auth.next-url";
+
+#[derive(Template)]
+#[template(path = "login.html")]
+pub struct LoginTemplate {
+    pub message: Option<String>,
+    pub next: Option<String>,
+}
+
+// This allows us to extract the "next" field from the query string. We use this
+// to redirect after log in.
+#[derive(Debug, Deserialize)]
+pub struct NextUrl {
+    next: Option<String>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Credentials {
+    pub client_id: String,
+    pub client_secret: String,
+}
+
+pub fn router() -> Router<()> {
+    Router::new()
+        .route("/login", post(self::post::login))
+        .route("/login", get(self::get::login))
+        .route("/logout", get(self::get::logout))
+}
+
+mod post {
+    use super::*;
+
+    pub async fn login(
+        auth_session: AuthSession,
+        session: Session,
+        Form(NextUrl { next }): Form<NextUrl>,
+    ) -> impl IntoResponse {
+        let (auth_url, csrf_state) = auth_session.backend.authorize_url();
+
+        session
+            .insert(CSRF_STATE_KEY, csrf_state.secret())
+            .expect("Serialization should not fail.");
+
+        session
+            .insert(NEXT_URL_KEY, next)
+            .expect("Serialization should not fail.");
+
+        Redirect::to(auth_url.as_str()).into_response()
+    }
+}
+
+mod get {
+    use super::*;
+
+    pub async fn login(Query(NextUrl { next }): Query<NextUrl>) -> LoginTemplate {
+        LoginTemplate {
+            message: None,
+            next,
+        }
+    }
+
+    pub async fn logout(mut auth_session: AuthSession) -> impl IntoResponse {
+        match auth_session.logout() {
+            Ok(_) => Redirect::to("/login").into_response(),
+            Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        }
+    }
+}
---
+++ examples/oauth2/src/web/oauth.rs
@@ -0,0 +1,75 @@
+use axum::{
+    extract::Query,
+    response::{IntoResponse, Redirect},
+    routing::get,
+    Router,
+};
+use http::StatusCode;
+use oauth2::CsrfToken;
+use serde::Deserialize;
+use tower_sessions::Session;
+
+use crate::{
+    users::{AuthSession, Credentials},
+    web::auth::{LoginTemplate, NEXT_URL_KEY},
+};
+
+pub const CSRF_STATE_KEY: &str = "oauth.csrf-state";
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct AuthzResp {
+    code: String,
+    state: CsrfToken,
+}
+
+pub fn router() -> Router<()> {
+    Router::new().route("/oauth/callback", get(self::get::callback))
+}
+
+mod get {
+    use super::*;
+
+    pub async fn callback(
+        mut auth_session: AuthSession,
+        session: Session,
+        Query(AuthzResp {
+            code,
+            state: new_state,
+        }): Query<AuthzResp>,
+    ) -> impl IntoResponse {
+        let Ok(Some(old_state)) = session.get(CSRF_STATE_KEY) else {
+            return StatusCode::BAD_REQUEST.into_response();
+        };
+
+        let creds = Credentials {
+            code,
+            old_state,
+            new_state,
+        };
+
+        let user = match auth_session.authenticate(creds).await {
+            Ok(Some(user)) => user,
+            Ok(None) => {
+                return (
+                    StatusCode::UNAUTHORIZED,
+                    LoginTemplate {
+                        message: Some("Invalid CSRF state.".to_string()),
+                        next: None,
+                    },
+                )
+                    .into_response()
+            }
+            Err(_) => return StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        };
+
+        if auth_session.login(&user).await.is_err() {
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        }
+
+        if let Ok(Some(next)) = session.remove::<String>(NEXT_URL_KEY) {
+            Redirect::to(&next).into_response()
+        } else {
+            Redirect::to("/").into_response()
+        }
+    }
+}
