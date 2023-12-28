009c5a29083f78928e79063e8dea13c6e6581de2
Max Countryman <maxc@me.com>
2023-12-28 07:11:03 -0800

    example  route formatting

--- examples/multi-auth/src/web/auth.rs
+++ examples/multi-auth/src/web/auth.rs
@@ -29,64 +29,68 @@ pub struct NextUrl {
 
 pub fn router() -> Router<()> {
     Router::new()
-        .route("/password_login", post(self::post::password_login))
-        .route("/oauth_login", post(self::post::oauth_login))
+        .route("/login/password", post(self::post::login::password))
+        .route("/login/oauth", post(self::post::login::oauth))
         .route("/login", get(self::get::login))
         .route("/logout", get(self::get::logout))
 }
 
 mod post {
     use super::*;
-    use crate::users::{Credentials, PasswordCreds};
 
-    pub async fn password_login(
-        mut auth_session: AuthSession,
-        Form(creds): Form<PasswordCreds>,
-    ) -> impl IntoResponse {
-        let user = match auth_session
-            .authenticate(Credentials::Password(creds.clone()))
-            .await
-        {
-            Ok(Some(user)) => user,
-            Ok(None) => {
-                return LoginTemplate {
-                    message: Some("Invalid credentials.".to_string()),
-                    next: creds.next,
+    pub(super) mod login {
+        use super::*;
+        use crate::users::{Credentials, PasswordCreds};
+
+        pub async fn password(
+            mut auth_session: AuthSession,
+            Form(creds): Form<PasswordCreds>,
+        ) -> impl IntoResponse {
+            let user = match auth_session
+                .authenticate(Credentials::Password(creds.clone()))
+                .await
+            {
+                Ok(Some(user)) => user,
+                Ok(None) => {
+                    return LoginTemplate {
+                        message: Some("Invalid credentials.".to_string()),
+                        next: creds.next,
+                    }
+                    .into_response()
                 }
-                .into_response()
+                Err(_) => return StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+            };
+
+            if auth_session.login(&user).await.is_err() {
+                return StatusCode::INTERNAL_SERVER_ERROR.into_response();
             }
-            Err(_) => return StatusCode::INTERNAL_SERVER_ERROR.into_response(),
-        };
 
-        if auth_session.login(&user).await.is_err() {
-            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+            if let Some(ref next) = creds.next {
+                Redirect::to(next).into_response()
+            } else {
+                Redirect::to("/").into_response()
+            }
         }
 
-        if let Some(ref next) = creds.next {
-            Redirect::to(next).into_response()
-        } else {
-            Redirect::to("/").into_response()
+        pub async fn oauth(
+            auth_session: AuthSession,
+            session: Session,
+            Form(NextUrl { next }): Form<NextUrl>,
+        ) -> impl IntoResponse {
+            let (auth_url, csrf_state) = auth_session.backend.authorize_url();
+
+            session
+                .insert(CSRF_STATE_KEY, csrf_state.secret())
+                .await
+                .expect("Serialization should not fail.");
+
+            session
+                .insert(NEXT_URL_KEY, next)
+                .await
+                .expect("Serialization should not fail.");
+
+            Redirect::to(auth_url.as_str()).into_response()
         }
-    }
-
-    pub async fn oauth_login(
-        auth_session: AuthSession,
-        session: Session,
-        Form(NextUrl { next }): Form<NextUrl>,
-    ) -> impl IntoResponse {
-        let (auth_url, csrf_state) = auth_session.backend.authorize_url();
-
-        session
-            .insert(CSRF_STATE_KEY, csrf_state.secret())
-            .await
-            .expect("Serialization should not fail.");
-
-        session
-            .insert(NEXT_URL_KEY, next)
-            .await
-            .expect("Serialization should not fail.");
-
-        Redirect::to(auth_url.as_str()).into_response()
     }
 }
 
