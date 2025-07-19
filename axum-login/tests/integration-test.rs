00383c744a15537202c7ce1b2590d52a0816630c
Max Countryman <maxc@me.com>
2025-07-19 09:54:23 -0700

    cookie jar rework

--- axum-login/tests/integration-test.rs
+++ axum-login/tests/integration-test.rs
@@ -1,16 +1,116 @@
 use std::{
     collections::HashMap,
     process::{Child, Command},
+    sync::Arc,
     time::{Duration, Instant},
 };
 
 use reqwest::{
     cookie::{CookieStore, Jar},
-    Client, StatusCode,
+    Client, StatusCode, Url,
 };
 use serial_test::serial;
 
 const WEBSERVER_URL: &str = "http://localhost:3000";
+
+#[tokio::test]
+#[serial]
+async fn sqlite_example() {
+    let _child_guard = start_example_binary("example-sqlite").await;
+
+    let cookie_jar = Arc::new(Jar::default());
+    let client = Client::builder()
+        .cookie_provider(cookie_jar.clone())
+        .build()
+        .unwrap();
+
+    // A logged out user is redirected to the login URL with a next query
+    //string.
+    let res = client.get(url("/")).send().await.unwrap();
+    assert_eq!(*res.url(), url("/login?next=%2F"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log in with invalid credentials.
+    let res = login(&client, "ferris", "bogus").await;
+    assert_eq!(*res.url(), url("/login"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log in with valid credentials.
+    let res = login(&client, "ferris", "hunter42").await;
+    assert_eq!(*res.url(), url("/"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log out and check the cookie has been removed in response.
+    let res = client.get(url("/logout")).send().await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
+    assert_eq!(
+        cookie_jar.cookies(&url("/")).iter().len(),
+        0,
+        "Expected 'id' cookie to be removed"
+    );
+}
+
+#[tokio::test]
+#[serial]
+async fn permissions_example() {
+    let _child_guard = start_example_binary("example-permissions").await;
+
+    let cookie_jar = Arc::new(Jar::default());
+    let client = Client::builder()
+        .cookie_provider(cookie_jar.clone())
+        .build()
+        .unwrap();
+
+    // A logged out user is redirected to the login URL with a next query string.
+    let res = client.get(url("/")).send().await.unwrap();
+
+    assert_eq!(*res.url(), url("/login?next=%2F"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log in with invalid credentials.
+    let res = login(&client, "ferris", "bogus").await;
+
+    assert_eq!(*res.url(), url("/login"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log in with valid credentials.
+    let res = login(&client, "ferris", "hunter42").await;
+
+    assert_eq!(*res.url(), url("/"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Try to access restricted page.
+    let res = client.get(url("/restricted")).send().await.unwrap();
+    assert_eq!(*res.url(), url("/login?next=%2Frestricted"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log in with valid credentials.
+    let res = login(&client, "admin", "hunter42").await;
+
+    assert_eq!(*res.url(), url("/"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    let cookies = cookie_jar.cookies(&url("/")).unwrap();
+    assert!(
+        cookies.to_str().unwrap_or("").contains("id="),
+        "Expected 'id' cookie to be set after login"
+    );
+
+    // Now we should be able to access the restricted page.
+    let res = client.get(url("/restricted")).send().await.unwrap();
+    assert_eq!(*res.url(), url("/restricted"));
+    assert_eq!(res.status(), StatusCode::OK);
+
+    // Log out and check the cookie has been removed in response.
+    let res = client.get(url("/logout")).send().await.unwrap();
+    assert_eq!(res.status(), StatusCode::OK);
+
+    assert_eq!(
+        cookie_jar.cookies(&url("/")).iter().len(),
+        0,
+        "Expected 'id' cookie to be removed"
+    );
+}
 
 struct ChildGuard {
     child: Child,
@@ -51,161 +151,18 @@ async fn start_example_binary(binary_name: &str) -> ChildGuard {
     ChildGuard { child }
 }
 
-//#[tokio::test]
-//#[serial]
-//async fn sqlite_example() {
-//    let _child_guard = start_example_binary("example-sqlite").await;
-//
-//    let client = Client::builder().cookie_store(true).build().unwrap();
-//
-//    // A logged out user is redirected to the login URL with a next query
-// string.    let res = client.get(WEBSERVER_URL).send().await.unwrap();
-//    assert_eq!(
-//        res.url().to_string(),
-//        format!("{WEBSERVER_URL}/login?next=%2F")
-//    );
-//
-//    // Log in with invalid credentials.
-//    let mut form = HashMap::new();
-//    form.insert("username", "ferris");
-//    form.insert("password", "bogus");
-//    let res = client
-//        .post(format!("{WEBSERVER_URL}/login"))
-//        .form(&form)
-//        .send()
-//        .await
-//        .unwrap();
-//    assert_eq!(res.url().to_string(), format!("{WEBSERVER_URL}/login"));
-//
-//    // Log in with valid credentials.
-//    let mut form = HashMap::new();
-//    form.insert("username", "ferris");
-//    form.insert("password", "hunter42");
-//    let res = client
-//        .post(format!("{WEBSERVER_URL}/login"))
-//        .form(&form)
-//        .send()
-//        .await
-//        .unwrap();
-//    assert_eq!(res.url().to_string(), format!("{WEBSERVER_URL}/"));
-//
-//    // Log out and check the cookie has been removed in response.
-//    let res = client
-//        .get(format!("{WEBSERVER_URL}/logout"))
-//        .send()
-//        .await
-//        .unwrap();
-//    let deleted_cookie = res.headers().get_all("set-cookie").iter().any(|val|
-// {        val.to_str().unwrap_or("").contains("id=")
-//            && val.to_str().unwrap_or("").contains("Max-Age=0")
-//    });
-//
-//    assert!(deleted_cookie, "Expected 'id' cookie to be removed");
-//}
+fn url(path: &str) -> Url {
+    let formatted_url = if path.starts_with('/') {
+        format!("{WEBSERVER_URL}{}", path)
+    } else {
+        format!("{WEBSERVER_URL}/{}", path)
+    };
+    formatted_url.parse().unwrap()
+}
 
-#[tokio::test]
-#[serial]
-async fn permissions_example() {
-    let _child_guard = start_example_binary("example-permissions").await;
-
-    let cookie_jar = std::sync::Arc::new(Jar::default());
-    let client = Client::builder()
-        .cookie_provider(cookie_jar.clone())
-        .build()
-        .unwrap();
-
-    // A logged out user is redirected to the login URL with a next query string.
-    let res = client.get(WEBSERVER_URL).send().await.unwrap();
-    assert_eq!(
-        res.url().to_string(),
-        format!("{WEBSERVER_URL}/login?next=%2F")
-    );
-    assert_eq!(res.status(), StatusCode::OK);
-    dbg!(res.headers().get_all("set-cookie"));
-    let url = WEBSERVER_URL.parse().unwrap();
-    for cookie in cookie_jar.cookies(&url).iter() {
-        eprintln!("Client cookie: {}", cookie.to_str().unwrap());
-    }
-    // Log in with invalid credentials.
+async fn login(client: &Client, username: &str, password: &str) -> reqwest::Response {
     let mut form = HashMap::new();
-    form.insert("username", "ferris");
-    form.insert("password", "bogus");
-    let res = client
-        .post(format!("{WEBSERVER_URL}/login"))
-        .form(&form)
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(res.url().to_string(), format!("{WEBSERVER_URL}/login"));
-    assert_eq!(res.status(), StatusCode::OK);
-    dbg!(res.headers().get_all("set-cookie"));
-
-    // Log in with valid credentials.
-    let mut form = HashMap::new();
-    form.insert("username", "ferris");
-    form.insert("password", "hunter42");
-    let res = client
-        .post(format!("{WEBSERVER_URL}/login"))
-        .form(&form)
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(res.url().to_string(), format!("{WEBSERVER_URL}/"));
-    assert_eq!(res.status(), StatusCode::OK);
-    dbg!(res.headers().get_all("set-cookie"));
-
-    let url = WEBSERVER_URL.parse().unwrap();
-    for cookie in cookie_jar.cookies(&url).iter() {
-        eprintln!("Client cookie: {}", cookie.to_str().unwrap());
-    }
-
-    // Try to access restricted page.
-    let res = client
-        .get(format!("{WEBSERVER_URL}/restricted"))
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(
-        res.url().to_string(),
-        format!("{WEBSERVER_URL}/login?next=%2Frestricted")
-    );
-    assert_eq!(res.status(), StatusCode::OK);
-    dbg!(res.headers().get_all("set-cookie"));
-
-    // Log in with valid credentials.
-    let mut form = HashMap::new();
-    form.insert("username", "admin");
-    form.insert("password", "hunter42");
-    let res = client
-        .post(format!("{WEBSERVER_URL}/login"))
-        .form(&form)
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(res.url().to_string(), format!("{WEBSERVER_URL}/"));
-    assert_eq!(res.status(), StatusCode::OK);
-    dbg!(res.headers().get_all("set-cookie"));
-
-    // Now we should be able to access the restricted page.
-    let res = client
-        .get(format!("{WEBSERVER_URL}/restricted"))
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(res.url().to_string(), format!("{WEBSERVER_URL}/restricted"));
-    assert_eq!(res.status(), StatusCode::OK);
-
-    // Log out and check the cookie has been removed in response.
-    let res = client
-        .get(format!("{WEBSERVER_URL}/logout"))
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(res.status(), StatusCode::OK);
-
-    assert_eq!(
-        cookie_jar.cookies(&url).iter().len(),
-        0,
-        "Expected 'id' cookie to be removed"
-    );
+    form.insert("username", username);
+    form.insert("password", password);
+    client.post(url("/login")).form(&form).send().await.unwrap()
 }
